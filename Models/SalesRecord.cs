7f6473e06d990e7812ca1507ee20c03329fb0fc8
Thieres Caetano <thieres.andrade@codenapp.com>
2023-01-03 19:18:05 -0300

    ajustando variáveis

--- Models/SalesRecord.cs
+++ Models/SalesRecord.cs
@@ -17,10 +17,10 @@ namespace SalesWebMvc.Models
 
         }
 
-        public SalesRecord(int id, DateTime data, double amount, SaleStatus status, Seller seller)
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
         {
             Id = id;
-            Data = data;
+            Date = date;
             Amount = amount;
             Status = status;
             Seller = seller;
